55b0daee2039e8f68fa9ab2b0ebf5b6589b7cae1
Student User <student@workstation.lab.example.com>
2021-10-01 08:30:29 -0400

    new commit

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,8 +1,4 @@
 <?php
 <<<<<<< HEAD
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change raghu test2 is a coming!\n";
-=======
-print "Hello, World! test php version is " . PHP_VERSION . "\n";
->>>>>>> 3108612fd51fa6bb8539f57a29f888a5fdcf2d28
 ?>
